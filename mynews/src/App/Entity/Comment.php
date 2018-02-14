bef2bff3b580edc7f03e544a4fb4e66f4c0b180e
Mike Nykytenko <ur7ez@ukr.net>
2018-02-14 17:37:13 +0200

    PHP Academy module-4 (mynews.com): fixed - top5 news included inactive news

--- mynews/src/App/Entity/Comment.php
+++ mynews/src/App/Entity/Comment.php
@@ -61,27 +61,25 @@ class Comment extends Base
     public function listTopCommentedNews($limit = 5, $days_old = 30)
     {
         $limit_clause = ($limit > 0) ? ' LIMIT ' . $limit : '';
-        $sql = "
-        SELECT t1.news_id, title, date_created, count(t1.id) as comments_cnt, max(created) AS last_date, min(created) AS first_date
+        $sql = 'SELECT t1.news_id, title, date_created, count(t1.id) as comments_cnt, max(created) AS last_date, min(created) AS first_date
         FROM (
         SELECT comments.news_id, title, date_created, comments.id, comments.created, datediff(current_date, comments.created) AS T
-        FROM " . $this->getTableName() . " JOIN news n ON comments.news_id = n.id
-        WHERE datediff(current_date, comments.created) <= " . $days_old . ") as t1
+        FROM ' . $this->getTableName() . ' JOIN news n ON comments.news_id = n.id
+        WHERE datediff(current_date, comments.created) <= ' . $days_old . ' AND active != 0) as t1
         GROUP BY news_id
-        ORDER BY comments_cnt DESC, last_date DESC" . $limit_clause . ";";
+        ORDER BY comments_cnt DESC, last_date DESC' . $limit_clause . ';';
         return $this->conn->query($sql);
     }
 
     public function listTopCommentingUsers($limit = 5)
     {
         $limit_clause = ($limit > 0) ? ' LIMIT ' . $limit : '';
-        $sql = "
-        SELECT user_id, name as user_name, count(id) as comments_cnt, sum(likes_cnt) as likes, sum(dislikes_cnt) as dislikes 
+        $sql = 'SELECT user_id, name as user_name, count(id) as comments_cnt, sum(likes_cnt) as likes, sum(dislikes_cnt) as dislikes 
         FROM (
         SELECT user_id, c.id, likes_cnt, dislikes_cnt, name 
-        FROM comments c LEFT JOIN users u ON c.user_id = u.id WHERE active != 0) t1
+        FROM ' . $this->getTableName() . ' c LEFT JOIN users u ON c.user_id = u.id WHERE active != 0) t1
         GROUP BY user_id
-        ORDER BY comments_cnt DESC" . $limit_clause . ";";
+        ORDER BY comments_cnt DESC' . $limit_clause . ';';
         return $this->conn->query($sql);
     }
 }
